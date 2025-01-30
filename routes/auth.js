b6ac657e81a448f7836a1165f62dbfa28af6ba2b
Bohdan Tron <bohdan.tron@gmail.com>
2025-01-30 16:50:26 +0200

    style: shortened router call

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
-const express = require('express');
+const router = require('express').Router();
 const { login } = require('../controllers/auth');
 const validator = require('../validator');
-
-const router = express.Router();
 
 router.post('/login', validator.createUser, login);
 
